0e6f7f75d198c1d56290cf484f90f29dee91ad11
angelatovars <143138700+angelatovars@users.noreply.github.com>
2023-09-04 12:09:21 -0500

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+    int enteros [] ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        int i,j,aux
+//metodo burbija
+   for (i=0;i<1;i++){
+       for (j=0;j<1;i++)
+           if(enteros[j]>
+           enteros[j+1]){
+               aux = enteros[j]
+               enteros [j+1] =enteros[j];
+           }
+    
+    return 0;
+}
